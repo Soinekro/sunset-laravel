3aa21a16e9dd0830f49e829dd51885eab2458aee
Elder <echumaceroj@ucvvirtual.edu.pe>
2021-06-03 02:40:39 -0500

    validaciones a español

--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -24,7 +24,7 @@
                     <label for="nombres" class="block text-sm font-medium text-gray-700">Nombres </label>
                     <input type="text" name="nombres" id="nombres" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                         @error('nombres')
-                            <p><strong class=" font-mono text-red-500">{{$message}}</strong></p>
+                            <p><strong class=" font-mono text-red-500 text-xs">{{$message}}</strong></p>
                         @enderror
                     </div>
 
@@ -32,36 +32,28 @@
                     <label for="apellidos" class="block text-sm font-medium text-gray-700">Apellidos</label>
                     <input type="text" name="apellidos" id="apellidos" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                         @error('apellidos')
-                            <p><strong class=" font-mono text-red-500">{{$message}}</strong></p>
+                            <p><strong class=" font-mono text-red-500 text-xs">{{$message}}</strong></p>
                         @enderror
                     </div>
 
-                    <div class="col-span-6 sm:col-span-4">
+                    <div class="col-span-6">
                     <label for="email" class="block text-sm font-medium text-gray-700">Direccion de Correo</label>
                     <input type="text" name="email" id="email" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                     @error('email')
-                            <p><strong class=" font-mono text-red-500">{{$message}}</strong></p>
+                            <p><strong class=" font-mono text-red-500 text-xs">{{$message}}</strong></p>
                         @enderror
                     </div>
-                    <div class="col-span-6 sm:col-span-4">
+                    <div class="col-span-6">
                         <label for="mensaje" class="block text-sm font-medium text-gray-700">Mensaje</label>
                         <textarea type="text" rows="3" id="mensaje" name="mensaje"  class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                         </textarea>
-                    </div>
-
-                    <div class="col-span-6 sm:col-span-4">
-                        <label for="ciudad" class="block text-sm font-medium text-gray-700">ciudad</label>
-                        <input type="text" name="ciudad" id="ciudad" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                            @error('ciudad')
-                            <p><strong class=" font-mono text-red-500">{{$message}}</strong></p>
-                            @enderror
                     </div>
 
                     <div class="col-span-6">
                     <label for="direccion" class="block text-sm font-medium text-gray-700">Direccion</label>
                     <input type="text" name="direccion" id="direccion" autocomplete="street-address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                         @error('direccion')
-                            <p><strong class=" font-mono text-red-500">{{$message}}</strong></p>
+                            <p><strong class=" font-mono text-red-500 text-xs">{{$message}}</strong></p>
                         @enderror
                     </div>
 
@@ -70,14 +62,21 @@
                     <label for="provincia" class="block text-sm font-medium text-gray-700">Provincia</label>
                     <input type="text" name="provincia" id="provincia" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                         @error('provincia')
-                            <p><strong class=" font-mono text-red-500">{{$message}}</strong></p>
+                            <p><strong class=" font-mono text-red-500 text-xs">{{$message}}</strong></p>
                         @enderror
-            </div>
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                    <label for="ciudad" class="block text-sm font-medium text-gray-700">Ciudad</label>
+                    <input type="text" name="ciudad" id="ciudad" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                        @error('ciudad')
+                            <p><strong class=" font-mono text-red-500 text-xs">{{$message}}</strong></p>
+                        @enderror
+                    </div>
 
                 </div>
                 </div>
                 <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Enviar
                 </button>
                 </div>
